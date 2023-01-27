598ed753ecf789d1f38f198aed8eda2cd98d331f
Hans Ellis <bullhorn@gmail.com>
2023-01-26 22:08:41 -0800

    update index view and home controller to view all entries in the database

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bakery.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bakery.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly BakeryContext _db;
+
+		public HomeController(BakeryContext db)
+		{
+			_db = db;
+		}
+
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
-			return View();
+			Flavor[] flv = _db.Flavors.ToArray();
+			Treat[] trt = _db.Treats.ToArray();
+			Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+			model.Add("flavors", flv);
+			model.Add("treats", trt);
+			return View(model);
 		}
 	}
 }
